e6027f6ee1c7d034127fc0b966490a052400ddbd
Flam <andrew@btv.lv>
2014-02-04 11:32:38 +0200

    Moved "Hello World" to string hello.

--- src/MyPackage/HelloWorldClass.java
+++ src/MyPackage/HelloWorldClass.java
@@ -3,8 +3,8 @@ package MyPackage;
 public class HelloWorldClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		String hello = "Hello World";
+		System.out.println(hello);
 	}
 
 }
